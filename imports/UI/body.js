9fcce3dc6d16646de4f86406f4fd1550f7186d89
RezaRahemtola <rahemtola.reza@gmail.com>
2020-06-29 18:38:34 +0200

    reproduction of the WordPress model started

---
+++ imports/UI/body.js
@@ -0,0 +1,9 @@
+// Useful imports
+import { Meteor } from 'meteor/meteor';
+import { Template } from 'meteor/templating';
+
+// HTML import
+import './body.html';
+
+// CSS import
+import './css/generic.css';
