00124f89d154d12a833eac61d6b3a858ad2ed8fc
RezaRahemtola <rahemtola.reza@gmail.com>
2020-06-29 19:40:14 +0200

    columns added for categories

--- imports/UI/body.js
+++ imports/UI/body.js
@@ -7,3 +7,14 @@ import './body.html';
 
 // CSS import
 import './css/generic.css';
+
+
+
+// jQuery code to display nav menu when burger-menu is clicked (code from https://bulma.io/documentation/components/navbar/)
+$(document).ready(function(){
+    // Check for click events on the navbar burger icon
+    $(".navbar-burger").click(function(){
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        $(".navbar-burger, .navbar-menu").toggleClass("is-active");
+    });
+});
